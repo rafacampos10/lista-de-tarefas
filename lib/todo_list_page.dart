9afc9997766c1e1e1e3ba37535044346448cfc32
Rafa Campos <rafinhasep94@hotmail.com>
2022-02-17 11:56:02 -0300

    upgrade no aplicativo lista de tarefas

--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -8,8 +8,73 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  TextEditingController _textEditingController = TextEditingController();
+  List<String> tarefas = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Lista de Tarefas"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          children: [
+            TextField( //input de dados
+              controller: _textEditingController,
+            ),
+            Container(
+              height:400,
+            child: ListView.separated(
+              separatorBuilder: (context, index) => Divider(
+
+              ), //faz a divisão e separando
+              itemCount: tarefas.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(tarefas[index]),
+                  onLongPress: (){ // remove um determinado item
+                    setState(() {
+                      tarefas.removeAt(index);
+                    });
+                  },
+                );
+              },
+            )
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton:Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+      FloatingActionButton(
+        backgroundColor: Colors.greenAccent,
+        onPressed:  (){
+          if(_textEditingController.text.length > 0){
+            setState(() {
+              tarefas.add(_textEditingController.text);
+            });
+            _textEditingController.clear();
+          }
+        },
+        child: Icon(Icons.add),
+      ), //botão de adicionar
+      FloatingActionButton(
+      backgroundColor: Colors.red,
+      onPressed:  (){
+          setState(() {
+            tarefas = [];
+          });
+          _textEditingController.clear();
+      },
+      child: Icon(Icons.clear),
+    ),//botão de limpar
+        ],
+      ),
+    );
   }
 }
